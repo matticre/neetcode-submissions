fb37bc7cbeab5d03e35a19d166e7419f78518691
Mattia Cretarola <matticre@protonmail.com>
2026-07-27 07:20:35 +0200

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int water = 0;
+
+        int left   = 0;
+        int right = height.size() - 1;
+        int max_left = height[left], max_right = height[right];
+
+        while (left < right){
+            if (max_left < max_right){
+                left++;
+                max_left = max(max_left, height[left]);
+                water += max_left - height[left];
+            } else {
+                right--;
+                max_right = max(max_right, height[right]);
+                water += max_right - height[right];
+            }
+        }
+
+        return water;
+    }
+};
